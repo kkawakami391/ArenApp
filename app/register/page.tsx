bb4da1c4eda64f92eb5246a8d438b2c68e9ea977
kkawakami391 <kkawakami391@gmail.com>
2025-04-26 09:19:47 +0900

    Testing layout

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="grid grid-cols-2 justify-items-center">
+      <div className="w-40 bg-blue-400">Milk</div>
+      <div>Weight</div>
+      <div>Height</div>
+      <div>Humor</div>
+      <div>Temperature</div>
+      <div>Toilet</div>
+    </div>
+  );
 }
 
 export default Register;
